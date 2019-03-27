25c567a125f20f243b6789d3842d547a1c5cb2af
IDTitanium <idriseun222@gmail.com>
2019-03-27 23:30:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ error_reporting(0);
 <html>
 <!DOCTYPE html>
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/style.css">
 <!--<link rel="stylesheet" href="font-awesome.min.css">-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
