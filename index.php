6cb4f0601fa5859d040b859cb4b70e2eccedff0c
IDTitanium <idriseun222@gmail.com>
2019-03-27 23:36:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,16 @@ error_reporting(0);
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <!--<link rel="stylesheet" href="bootstrap.min.css">-->
 <script src="js/index.js"></script>
-<script src="js/require.js"></script>
-<script src="js/mailer.js"></script>
+<!-- <script src="js/require.js"></script> -->
+<!-- <script src="js/mailer.js"></script> -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <title>Idris Lawal</title>
 </head>
 
@@ -63,7 +71,7 @@ error_reporting(0);
 <input type="submit" class="btn" value="Send Message to Idris" name="submit">
 </form>
 
-
+<!-- 
 <div id="background-wrap">
     <div class="bubble x1"></div>
     <div class="bubble x2"></div>
@@ -79,7 +87,7 @@ error_reporting(0);
     <div class="bubble x6"></div>
     <div class="bubble x7"></div>
     <div class="bubble x8"></div>
-</div>
+</div> -->
 </body>
 
 </html>
